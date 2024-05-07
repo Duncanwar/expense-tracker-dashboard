91a306f8c476657fd0fe7220a2b860af41e7254c
Duncanwar <semunda9@gmail.com>
2024-05-07 18:01:31 +0200

    react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,24 @@
 import { useMemo } from "react"
 import { createTheme } from "@mui/material/styles"
 import { themeSettings } from "./theme"
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   const theme = useMemo(()=> createTheme(themeSettings),[])
   return (
       <div className="app">
-        <BrowserRo
+        <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline/>
-          Hi
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route />
+            </Routes>
+          </Box>
         </ThemeProvider>
+        </BrowserRouter>
       </div>
   )
 }
