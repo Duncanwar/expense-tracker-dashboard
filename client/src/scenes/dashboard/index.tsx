9cbb6504586276c9e710a1c43991ae824b3fa837
Duncanwar <semunda9@gmail.com>
2024-05-09 17:19:06 +0200

    new

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -5,12 +5,24 @@ type Props = {};
 
 const gridTemplate=`
 "a b c "
+"a b c "
+"a b c "
+"a b f "
+"d e f "
+"d e f "
+"d h i "
+"g h i "
+"g h j "
+"g h j "
 `
 
 const Dashboard = (props: Props) => {
   const { palette } = useTheme();
   return (
-    <Box width="100%" height="100%" sx={{ gridTemplateAreas: gridTemplate }}>
+    <Box width="100%" height="100%" sx={{ 
+      gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+      gridTemplateRows
+      gridTemplateAreas: gridTemplate }}>
       Dashboard
     </Box>
   );
