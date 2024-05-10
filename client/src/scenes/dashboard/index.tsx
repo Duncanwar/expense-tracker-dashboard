cae8306b7aa1a8a8753815eff919d1c421d8efda
Duncanwar <semunda9@gmail.com>
2024-05-10 17:17:21 +0200

    layout

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -19,11 +19,23 @@ const gridTemplate=`
 const Dashboard = (props: Props) => {
   const { palette } = useTheme();
   return (
-    <Box width="100%" height="100%" sx={{ 
+    <Box width="100%" height="100%"
+    display="grid"
+    gap="1.5rem"
+     sx={{ 
       gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-      gridTemplateRows
+      gridTemplateRows: "repeat(3, minmax(60px, 1fr))",
       gridTemplateAreas: gridTemplate }}>
-      Dashboard
+      <Box bgcolor="#fff" gridArea="a"> </Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
     </Box>
   );
 };
