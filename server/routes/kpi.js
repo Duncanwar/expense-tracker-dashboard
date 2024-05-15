dc03e95bea664a025b04dc6973af888d78bee7e0
Duncanwar <semunda9@gmail.com>
2024-05-15 18:53:27 +0200

    kpi routes:'

---
+++ server/routes/kpi.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router()
+
+router.get('/kpis', async(req,res)=>{
+    try {
+        const kpis =  await KPI
+    } catch (error) {
+        
+    }
+})
