2f3a0d7ce1558979145cd495f3af2e2b318fcf6f
Duncanwar <semunda9@gmail.com>
2024-05-15 17:29:35 +0200

    mongo installation

--- server/index.js
+++ server/index.js
@@ -18,5 +18,12 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 
 /* MONGOOSE SETUP */
-const PORT =  process.env.PORT ||
-S0ctPXcjLhxovbtv : cluster finance
+const PORT =  process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(async ()=>{
+    app.listen(PORT,()=>console.log(`Server Port:${PORT}`));
+})
+.catch((error)=>console.log(`${error} did not connect`))
+// S0ctPXcjLhxovbtv : cluster finance
